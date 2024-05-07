9276918027d41d9b40f47b617a56c6c94f95483b
Emanuel Rigo <112441019+EmanuelRigo@users.noreply.github.com>
2024-05-07 17:53:59 -0300

    agregando funcion createTask

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,33 +8,36 @@ import ProfilePage from "./pages/ProfilePage";
 import HomePage from "./pages/HomePage";
 
 import ProtectedRoute from "./ProtectedRoute";
+import { TaskProvider } from "./context/TasksContext";
 
 const App = () => {
   return (
     <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage></HomePage>}></Route>
-          <Route path="/login" element={<LoginPage />}></Route>
-          <Route path="/register" element={<RegisterPage />}></Route>
+      <TaskProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage></HomePage>}></Route>
+            <Route path="/login" element={<LoginPage />}></Route>
+            <Route path="/register" element={<RegisterPage />}></Route>
 
-          <Route element={<ProtectedRoute></ProtectedRoute>}>
-            <Route path="/tasks" element={<TasksPage></TasksPage>}></Route>
-            <Route
-              path="/add-task"
-              element={<TaskFormPage></TaskFormPage>}
-            ></Route>
-            <Route
-              path="/task/:id"
-              element={<TaskFormPage></TaskFormPage>}
-            ></Route>
-            <Route
-              path="/profile"
-              element={<ProfilePage></ProfilePage>}
-            ></Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
+            <Route element={<ProtectedRoute></ProtectedRoute>}>
+              <Route path="/tasks" element={<TasksPage></TasksPage>}></Route>
+              <Route
+                path="/add-task"
+                element={<TaskFormPage></TaskFormPage>}
+              ></Route>
+              <Route
+                path="/task/:id"
+                element={<TaskFormPage></TaskFormPage>}
+              ></Route>
+              <Route
+                path="/profile"
+                element={<ProfilePage></ProfilePage>}
+              ></Route>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </TaskProvider>
     </AuthProvider>
   );
 };
--- client/src/pages/TasksPage.jsx
+++ client/src/pages/TasksPage.jsx
@@ -1,10 +1,25 @@
-import { useAuth } from "../context/AuthContext";
+import { useTask } from "../context/TasksContext";
+import { useEffect } from "react";
 
 const TasksPage = () => {
-  const { user } = useAuth();
-  console.log(user);
+  const { getTasks, tasks } = useTask();
 
-  return <div>TasksPage</div>;
+  useEffect(() => {
+    getTasks();
+  }, []);
+
+  if (tasks.length === 0) return <h1>no hay tareas</h1>;
+
+  return (
+    <div>
+      {tasks.map((task) => (
+        <div key={task._id}>
+          <h1>{task.title} </h1>
+          <p>{task.description}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default TasksPage;
